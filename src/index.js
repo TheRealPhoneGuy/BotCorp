0ab28ce85f3883d33f7e0b8acc5da5a6835c6b68
= <=>
2025-04-26 23:47:13 +0200

    Ticket 1

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ require("dotenv").config();
 
 client.login(process.env.Token);
 client.commands = new Collection();
+client.interactions = new Collection();
 client.color = "Blue";
 
 require("./handlers/registers")(client);
