eda86a114624aa829ec622f2ea81e66d4c4959bd
= <=>
2025-04-26 19:23:12 +0200

    First loggin

--- src/index.js
+++ src/index.js
@@ -1,4 +1,17 @@
-const { Client, IntentsBitField } = require("discord.js")
-const client = new Client({ intents: [IntentsBitField.Flags.AutoModerationConfiguration] })
+const { Client, IntentsBitField } = require("discord.js"),
+  colors = require("colors");
+const client = new Client({ intents: 53608447 });
+require("dotenv").config();
 
-client.login()
+client.login(process.env.Token);
+client.color = "Blue";
+
+client.once("ready", () => {
+  console.log(
+    colors.white(
+      `Connectée en tant que ${colors[`bright` + client.color](
+        client.user.tag
+      )}.`
+    )
+  );
+});
